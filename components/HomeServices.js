77584155a54ee776c7a94311d15a6aff04611bf1
Ian Cahn <iancahn@live.com>
2022-12-26 11:50:56 +0200

    Our-service styling
    Styled first 3 services

--- components/HomeServices.js
+++ components/HomeServices.js
@@ -2,6 +2,7 @@ import Image from 'next/image';
 
 
 import brandDesign from '../public/assets/brand-design-services.jpg';
+import rightArrow from '../public/assets/right-arrow.jpg';
 // import Animation
 // import { motion } from "framer-motion";
 // import components
@@ -23,7 +24,23 @@ function HomeServices() {
                         <div className="work-block-text">
                             <h4 className="service-name">Brand Design</h4>
                             <p>Every great company starts with a brilliant brand design. A professional brand design works across all the touchpoints of your brand. These include logo design, branding elements and so much more.</p>
-                            <button className="black-btn2">View Service</button>
+                            <p className="service-item-list">
+                                <ul>
+                                    <li>Item 1</li>
+                                    <li>Item 1</li>
+                                    <li>Item 1</li>
+                                </ul>
+                            </p>
+                            <div className="service-links">
+                                <button className="service-plans-button">See Plans</button>
+                                <Image
+                                    src={rightArrow}
+                                    width={25}
+                                    height={19}
+                                    alt="An arrow pointing towards the right"
+                                    className="link-arrow"
+                                />
+                            </div>
                         </div>
                     </div>
                     <div className="work-block">
@@ -37,7 +54,23 @@ function HomeServices() {
                         <div className="work-block-text">
                             <h4 className="service-name">Website Design</h4>
                             <p>Your company's website is the very first impression potential clients get of your company. Get your website professionally designed using the latest web technologies and mobile friendly and responsive design styles.</p>
-                            <button className="black-btn2">View Service</button>
+                            <p className="service-item-list">
+                                <ul>
+                                    <li>Item 1</li>
+                                    <li>Item 1</li>
+                                    <li>Item 1</li>
+                                </ul>
+                            </p>
+                            <div className="service-links">
+                                <button className="service-plans-button">See Plans</button>
+                                <Image
+                                    src={rightArrow}
+                                    width={25}
+                                    height={19}
+                                    alt="An arrow pointing towards the right"
+                                    className="link-arrow"
+                                />
+                            </div>
                         </div>
                     </div>
                     <div className="work-block">
@@ -51,7 +84,23 @@ function HomeServices() {
                         <div className="work-block-text">
                             <h4 className="service-name">Digital Marketing</h4>
                             <p>Once your new brand and website has been designed, its time to get it infront of potential customers. We use our wide range of digital marketing to generate leads and convert online visitors into customers for our clients.</p>
-                            <button className="black-btn2">View Service</button>
+                            <p className="service-item-list">
+                                <ul>
+                                    <li>Item 1</li>
+                                    <li>Item 1</li>
+                                    <li>Item 1</li>
+                                </ul>
+                            </p>
+                            <div className="service-links">
+                                <button className="service-plans-button">See Plans</button>
+                                <Image
+                                    src={rightArrow}
+                                    width={25}
+                                    height={19}
+                                    alt="An arrow pointing towards the right"
+                                    className="link-arrow"
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
