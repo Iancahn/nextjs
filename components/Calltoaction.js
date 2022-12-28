f9628649b913b9da02bbc3b572db55948a33604c
Ian Cahn <iancahn@live.com>
2022-12-28 12:16:45 +0200

    Bottom CTA styled
    + New colors
    + Added Images
    + Footer Styling

--- components/Calltoaction.js
+++ components/Calltoaction.js
@@ -1,8 +1,26 @@
+import Image from 'next/image';
+
+import ctaMobile from "../public/assets/CTA-phone.png"
+
 function Calltoaction() {
     return (
         <div className="call-to-action">
-            <h3 className="big-h3">Have some questions for us?</h3>
-            <button className="call-to-action-button">Let's chat</button>
+            <div className="container">
+                <div className="row">
+                    <div className="column-no-padding centered3">
+                        <Image
+                            src={ctaMobile}
+                            width={533}
+                            height={505}
+                            alt="Get in touch with PixelByPixel"
+                        />
+                    </div>
+                    <div className="column centered4">
+                        <h3 className="big-h3 black-font">Let's start the conversation.</h3>
+                        <button className="call-to-action-button">Contact Us Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
